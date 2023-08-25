5ae77ed1e2e53c13f8d17089c21f110e53ce24a4
haris8899 <harisraza.xperience@gmail.com>
2023-08-25 16:17:14 +0500

    Api Initialized

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -6,5 +6,5 @@ import 'package:my_food_app/data/repository/main_product_repo.dart';
 Future<void> init() async {
   Get.lazyPut(() => ApiClient(AppBaseUrl: "url"));
   Get.lazyPut(() => MainProductRepo(apiClient: Get.find()));
-  Get.lazyPut(() => MainProductController(mainProductRepo: ));
+  Get.lazyPut(() => MainProductController(mainProductRepo: Get.find()));
 }
